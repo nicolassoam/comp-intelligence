df43e15f5109808c82c37ea33fd6c35c4984c3dd
nicolassoam <nsm4456@gmail.com>
2023-09-25 14:00:46 -0300

    incluindo informações do primeiro problema + instancias

---
+++ OPHS/include/graph.h
@@ -0,0 +1,8 @@
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <iostream>
+#include <vector>
+
+
+#endif  // GRAPH_H_
---
+++ OPHS/include/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+namespace util{
+    using k_int = std::vector<int>;
+
+    k_int read_instance(std::string filename){
+        int n_vertices, nextra_hotels, trips;
+        double tmax;
+        double t_lenght
+    };
+}
+
+#endif
