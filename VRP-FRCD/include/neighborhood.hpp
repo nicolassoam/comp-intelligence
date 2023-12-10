9b4809815656acf58c82948bdec5785d10255fd2
nicolassoam <nsm4456@gmail.com>
2023-12-10 19:35:51 -0300

    feat: retirando cucos e colocando veiculos para movimentos

--- VRP-FRCD/include/neighborhood.hpp
+++ VRP-FRCD/include/neighborhood.hpp
@@ -11,12 +11,12 @@
 
 namespace Neighborhood
 {
-    void shift10 (cuckoo& oldEgg, cuckoo& newEgg, Instance* inst);      // SHIFT(1,0)
-    void reinsertion (cuckoo& oldEgg, cuckoo& newEgg, Instance* inst);  // REINSERTION
-    void twoOpt (cuckoo& oldEgg, cuckoo& newEgg, Instance* inst);       // 2-OPT
-    void exchange (cuckoo& oldEgg, cuckoo& newEgg, Instance* inst);     // EXCHANGE 
-    void swap12 (cuckoo& oldEgg, cuckoo& newEgg, Instance* inst);       // SWAP(1,2)
-    void cross (cuckoo& oldEgg, cuckoo& newEgg, Instance* inst);        // CROSS
+    void shift10 (Vehicle &v1,Vehicle &v2, Instance* inst);      // SHIFT(1,0)
+    void reinsertion (Vehicle &v, Instance* inst);  // REINSERTION
+    void twoOpt (Vehicle &v, Instance* inst);       // 2-OPT
+    void exchange (Vehicle &v, Instance* inst);     // EXCHANGE 
+    void swap21 (Vehicle &v1,Vehicle &v2, Instance* inst);       // SWAP(1,2)
+    void cross (Vehicle &v1,Vehicle &v2, Instance* inst);        // CROSS
 }
 
 #endif // NEIGHBORHOOD_HPP_
